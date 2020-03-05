7e83c79f25f3d3c551334e591b9ae7524327afa9
Varun Barad <varun@varunbarad.com>
2020-03-05 23:07:15 +0530

    Add check for views that become invisible due to parent-related constraints

---
+++ android-lint-checks/src/main/java/com/varunbarad/androidlintchecks/ConstraintLayoutChildNotVisibleDetector.kt
@@ -0,0 +1,57 @@
+package com.varunbarad.androidlintchecks
+
+import com.android.tools.lint.detector.api.*
+import org.w3c.dom.Element
+
+class ConstraintLayoutChildNotVisibleDetector : LayoutDetector() {
+    companion object {
+        @JvmStatic
+        internal val ISSUE_POSSIBLE_INVISIBLE_VIEW = Issue.create(
+            id = "PossibleInvisibleView",
+            briefDescription = "The constraints on this view might make it invisible.",
+            explanation = "This view will probably become invisible. Check all the constraints related to parent.",
+            category = Category.USABILITY,
+            priority = 8,
+            severity = Severity.WARNING,
+            implementation = Implementation(
+                ConstraintLayoutChildNotVisibleDetector::class.java,
+                Scope.ALL_RESOURCES_SCOPE
+            )
+        )
+
+        @JvmStatic
+        private val PARENT_CONSTRAINT_ATTRIBUTE_NAMES = listOf(
+            "app:layout_constraintLeft_toRightOf",
+            "app:layout_constraintRight_toLeftOf",
+            "app:layout_constraintStart_toEndOf",
+            "app:layout_constraintEnd_toStartOf",
+            "app:layout_constraintTop_toBottomOf",
+            "app:layout_constraintBottom_toTopOf"
+        )
+    }
+
+    override fun getApplicableElements(): Collection<String>? {
+        return listOf(
+            "androidx.constraintlayout.widget.ConstraintLayout",
+            "android.support.constraint.ConstraintLayout"
+        )
+    }
+
+    override fun visitElement(context: XmlContext, element: Element) {
+        for (childView in element.childNodes.toList()) {
+            childView.attributes
+                .toPairedNameValueList()
+                .filter() { attribute ->
+                    PARENT_CONSTRAINT_ATTRIBUTE_NAMES.contains(attribute.first)
+                }.forEach { attribute ->
+                    if (attribute.second == "parent") {
+                        context.report(
+                            issue = ISSUE_POSSIBLE_INVISIBLE_VIEW,
+                            location = context.getLocation(childView),
+                            message = ISSUE_POSSIBLE_INVISIBLE_VIEW.getExplanation(TextFormat.TEXT)
+                        )
+                    }
+                }
+        }
+    }
+}
--- android-lint-checks/src/main/java/com/varunbarad/androidlintchecks/LintRegistry.kt
+++ android-lint-checks/src/main/java/com/varunbarad/androidlintchecks/LintRegistry.kt
@@ -11,6 +11,7 @@ class LintRegistry : IssueRegistry() {
     override val issues: List<Issue>
         get() = listOf(
             InputTypeDetector.ISSUE_MISSING_INPUT_TYPE,
-            WebViewMetricsMetaTagDetector.ISSUE_MISSING_WEBVIEW_METRICS_META_TAG
+            WebViewMetricsMetaTagDetector.ISSUE_MISSING_WEBVIEW_METRICS_META_TAG,
+            ConstraintLayoutChildNotVisibleDetector.ISSUE_POSSIBLE_INVISIBLE_VIEW
         )
 }
