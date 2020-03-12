b3241b096da9c0279acfe0cb278a61aea3cb7cef
Varun Barad <varun@varunbarad.com>
2020-03-12 18:48:37 +0530

    Reformat issue names in registry

--- android-lint-checks/src/main/java/com/varunbarad/androidlintchecks/LintRegistry.kt
+++ android-lint-checks/src/main/java/com/varunbarad/androidlintchecks/LintRegistry.kt
@@ -3,6 +3,10 @@ package com.varunbarad.androidlintchecks
 import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.detector.api.CURRENT_API
 import com.android.tools.lint.detector.api.Issue
+import com.varunbarad.androidlintchecks.ConstraintLayoutChildNotVisibleDetector.Companion.ISSUE_POSSIBLE_INVISIBLE_VIEW
+import com.varunbarad.androidlintchecks.InputTypeDetector.Companion.ISSUE_MISSING_INPUT_TYPE
+import com.varunbarad.androidlintchecks.LogWtfDetector.Companion.ISSUE_LOG_WTF
+import com.varunbarad.androidlintchecks.WebViewMetricsMetaTagDetector.Companion.ISSUE_MISSING_WEBVIEW_METRICS_META_TAG
 
 class LintRegistry : IssueRegistry() {
     override val api: Int
@@ -10,9 +14,9 @@ class LintRegistry : IssueRegistry() {
 
     override val issues: List<Issue>
         get() = listOf(
-            InputTypeDetector.ISSUE_MISSING_INPUT_TYPE,
-            WebViewMetricsMetaTagDetector.ISSUE_MISSING_WEBVIEW_METRICS_META_TAG,
-            ConstraintLayoutChildNotVisibleDetector.ISSUE_POSSIBLE_INVISIBLE_VIEW,
-            LogWtfDetector.ISSUE_LOG_WTF
+            ISSUE_MISSING_INPUT_TYPE,
+            ISSUE_MISSING_WEBVIEW_METRICS_META_TAG,
+            ISSUE_POSSIBLE_INVISIBLE_VIEW,
+            ISSUE_LOG_WTF
         )
 }
